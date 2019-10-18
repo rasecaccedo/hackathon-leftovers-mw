338346e99ec3a11722f3be744b644fa85af01d78
Cesar <cesar.cruz@accedo.tv>
2019-10-18 12:33:40 +0200

    added audio endpoint

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000;
+const express = require('express');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const audio = require("./audio");
+const app = express();
+const port = process.env.PORT || 3001;
+
+const router = express.Router();
+
+router.use('/audio', audio);
+
+app.use(router);
+
+app.get('/image', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
